b45d88d76137c3d09e89652f3f96267710694bcf
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:17:49 +0200

    Add files via upload

---
+++ Lame_age_printer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lame_age_printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You look older then " + age);
+        }
+    }
+}
